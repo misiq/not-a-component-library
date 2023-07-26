54b8d7373d0c46c602087936dcb0adc87a8aee16
Michal Baran <michal.baran@live.com>
2023-07-26 22:41:42 +0200

    Add rollup

---
+++ src/components/atoms/button/Button.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Button = () => <button>HELLO WORLD BUTTON</button>;
+
+export default Button;
---
+++ src/components/atoms/link/link.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Link = () => <a href="http://google.pl">LINK</a>;
+
+export default Link;
